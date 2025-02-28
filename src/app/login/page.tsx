043f7000a634c1b1987f7034dc010cd55c53f45e
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-02-28 16:26:14 -0500

    Fix unescaped apostrophe in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -128,7 +128,7 @@ export default function LoginPage() {
         
         <CardFooter className="flex justify-center">
           <p className="text-sm text-gray-500">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <a href="/signup" className="text-blue-600 hover:underline">
               Sign up
             </a>
